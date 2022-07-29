931c12e37220c9f5aef92ce0347d949aa93ae823
brettl1991 <brettl1991@gmail.com>
2022-07-29 13:41:17 +0100

    Added our components

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+//This component is the top part: song, name, artist, picture
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;
