ea27d47c4a31a0cef281db347cda8fea3f9eb44f
brettl1991 <brettl1991@gmail.com>
2022-08-03 12:25:07 +0100

    Implement library ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 
 //Import style
 import "./styles/app.scss";
@@ -23,6 +24,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
