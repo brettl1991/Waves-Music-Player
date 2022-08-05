2b564ae8fb0746bc20c800c0b760df21138c527b
brettl1991 <brettl1991@gmail.com>
2022-08-05 16:24:39 +0100

    Add animation style

--- src/components/Player.js
+++ src/components/Player.js
@@ -83,11 +83,21 @@ const Player = ({
 
     playAudio(isPlaying, audioRef);
   };
+
+  //Add the styles
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`,
+  };
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <div className="track">
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+          }}
+          className="track"
+        >
           <input
             min={0}
             max={songInfo.duration || 0}
@@ -95,7 +105,7 @@ const Player = ({
             onChange={dragHandler}
             type="range"
           />
-          <div className="animate-track"></div>
+          <div style={trackAnim} className="animate-track"></div>
         </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
